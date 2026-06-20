0fc1d1243735ee78f1bd4bf592199a624716b1ac
Benjamin Stortroen <magmac495@gmail.com>
2026-06-19 21:33:34 -0500

    Add movement up and down + sway left and right to balloon

--- Scripts/balloon_vehicle.gd
+++ Scripts/balloon_vehicle.gd
@@ -9,28 +9,29 @@ extends Node3D
 var direction_up = true
 var can_move = true
 
+func _ready() -> void:
+	begin_up_down()
+	begin_sway()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_up_and_down()
+	pass
 
 # Use Tweens (lightweight animation keyframes) to move balloon up and down
-func move_up_and_down():
-	if direction_up and can_move:
-		var tween = create_tween()
-		tween.tween_property(self, "position", end_pos, 1.0).set_trans(Tween.TRANS_QUAD)
-		direction_up = !direction_up
-		can_move = false
-		await tween.finished
-		can_move = true
-	elif not direction_up and can_move:
-		var tween = create_tween()
-		tween.tween_property(self, "position", start_pos, 1.0).set_trans(Tween.TRANS_QUAD)
-		direction_up = !direction_up
-		can_move = false
-		await tween.finished
-		can_move = true
+func begin_up_down():
+	var tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "position", start_pos, 1.0).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "position", end_pos, 1.0).set_trans(Tween.TRANS_QUAD)
 	
-
+func begin_sway():
+	var tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "rotation:z", deg_to_rad(15), 1.0 )
+	tween.tween_property(self, "rotation:z", 0.0, 1.0 )
+	tween.tween_property(self, "rotation:z", deg_to_rad(-15), 1.0 )
+	tween.tween_property(self, "rotation:z", 0.0, 1.0 )
+	
 
 	
 	
